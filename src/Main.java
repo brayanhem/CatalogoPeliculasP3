0691c5b48ac4b1bd6a319362bfa605ff4bce7552
brayanhem <brayan.herrera@uabc.edu.mx>
2026-09-02 10:47:43 -0700

    Se realizo la clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    return;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Catalogo miCatalogo = new Catalogo();
+
+        Pelicula p1 = new Pelicula("", "Wachowski", 1999, "Acción", 136);
+        Pelicula p2 = new Pelicula("Interstelar", "Nolan", 2014, "Sci-Fi", 169);
+        Pelicula p3 = new Pelicula("Titanic", "Cameron", 1997, "Drama", 195);
+        //Se agregaran las peloculas al ccatalogo
+        miCatalogo.agregarPelicula(p1);
+        miCatalogo.agregarPelicula(p2);
+        miCatalogo.agregarPelicula(p3);
+        // Mostrar todas las películas
+        System.out.println("LISTA DE PELÍCULAS");
+        System.out.println(miCatalogo.getPelicula());
+
+        System.out.print("Escribe el título de la película que buscas: ");
+        String peliculaABuscar = entrada.nextLine();
+
+        miCatalogo.buscarPeliculaPorTitulo(peliculaABuscar);
+    }
 }
